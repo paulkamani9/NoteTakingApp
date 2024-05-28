832d8df007be86b0c2ea0ff38e1634d17dea450d
pakmmi <paulkamani9@gmail.com>
2024-05-28 10:31:37 +0400

    loads of bootstrap

---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,11 @@
+import NoteForm from "../components/NoteForm";
+
+const NewNote = () => {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  );
+};
+export default NewNote;
